ed4cc6c5f32479e35520728c621876ad8e19b327
Dan Kim <itsdanya@gmail.com>
2016-12-13 07:20:31 +0300

    Clean up tests

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -5,24 +5,24 @@ class UserSessionsControllerTest < ActionController::TestCase
     @user = users(:noam)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get :create
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     login_user
     get :destroy
     assert_redirected_to :users
-    assert_equal("Logged out!", flash[:notice])
+    assert_equal('Logged out!', flash[:notice])
   end
 
-  test "when logged out should not get destroy" do
+  test 'when logged out should not get destroy' do
     logout_user
     get :destroy
     assert_redirected_to root_url
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,3 +12,7 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include Sorcery::TestHelpers::Rails
 end
+
+class ActionController::TestCase
+  include Sorcery::TestHelpers::Rails::Controller
+end
