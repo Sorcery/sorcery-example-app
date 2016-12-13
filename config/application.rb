3063232ea42c7a36b3f919c11d6c99d07bf1fc43
Dan Kim <itsdanya@gmail.com>
2016-12-13 08:12:12 +0300

    Update config files

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,5 @@ module Collaboration
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
   end
 end
