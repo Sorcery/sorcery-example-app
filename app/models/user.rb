1ead76e6bf5855e17044f88e317effe4db3bc7f4
Dan Kim <itsdanya@gmail.com>
2016-12-13 07:21:35 +0300

    User: remove attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :providers_attributes
-  
   has_many :providers, :class_name => "UserProvider", :dependent => :destroy
   accepts_nested_attributes_for :providers
 
   authenticates_with_sorcery!
-  
+
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
   validates_confirmation_of :password, :message => "should match confirmation", :if => :password
 end
